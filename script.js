d66d6c7f5ba81d3abfe75159df6f2ebca88a5e34
gguneii <guneysuleymanofa@gmail.com>
2022-11-08 20:54:16 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const addBtn = document.querySelector('.plus-add');
+const ul = document.querySelector('ul');
+const upDown=document.querySelector('.up-down')
+let direction="";
+
+addBtn.addEventListener('click', () =>{
+  const li=document.createElement('li')
+  li.className="todo-item";
+  const input=document.createElement('input')
+  input.className="input"
+  const button=document.createElement('button');
+  button.className="x"
+
+  button.addEventListener('click', ()=>{
+    li.remove();
+  })
+  
+  li.appendChild(input);
+  li.appendChild(button);
+  ul.appendChild(li);
+
+})
+ 
+upDown.addEventListener('click',()=>{
+  toggleUpDown();
+  const todos = ul.childNodes;
+  const itemsArr = [];
+
+  for (var i in todos) {
+    if (todos[i].nodeType == 1) {
+      itemsArr.push(todos[i]);
+    }
+  }
+
+  itemsArr.sort((a, b) => compareFn(a, b, direction));
+  for (i = 0; i < itemsArr.length; ++i) {
+    ul.appendChild(itemsArr[i]);
+  }
+
+  
+})
+function toggleUpDown() {
+  if (direction) upDown.classList.remove(direction);
+  direction = direction === "asc" ? "desc" : "asc";
+  upDown.classList.add(direction);
+  upDown.classList.add("active");
+}
+
+  function compareFn(a, b, direction) {
+    let aValue = a.children[0]?.value;
+    let bValue = b.children[0]?.value;
+  
+    if (aValue > bValue) {
+      return direction === "asc" ? 1 : -1;
+    } else if (aValue < bValue) {
+      return direction === "asc" ? -1 : 1;
+    } else return 0;
+  }
+
